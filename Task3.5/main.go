6b49c02d10c6fc4ffb8194b9fad54a47b4c4eaa3
zelinskayas <sveta455@mail.ru>
2024-03-29 00:02:10 +0300

    Task3.5

---
+++ Task3.5/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		localConst1 = 1
+		localConst2 = 2
+		localConst3 = 3
+		localConst4 = 4
+		localConst5 = 5
+	)
+	fmt.Println(localConst1, localConst2, localConst3, localConst4, localConst5)
+}
